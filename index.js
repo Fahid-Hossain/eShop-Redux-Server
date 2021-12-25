82c6473bc01b9fd8d2e1217401ec61d97c98898a
Fahid Hossain <fahidhossain2021@gmail.com>
2021-12-25 13:54:20 +0600

    get api for load all data and single data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const cors = require('cors');
@@ -40,7 +40,22 @@ async function run() {
         
         })
 
-        //   console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        //Get api load all products
+        app.get("/products",async(req,res)=>{
+            const cursor = productsCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        //Get api load single data
+        app.get("/products/:id",async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)};
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        })
+
+      
     } finally {
         //   await client.close();
     }
